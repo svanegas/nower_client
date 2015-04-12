7241c4ee9340e066f28e4dda0a627211161a9639
Felipe Tovar <f.tovar12@hotmail.com>
2015-04-12 11:20:01 -0500

    Global variables are working correctly and we get the branches of any store that does a login

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -1,9 +1,10 @@
 //var app = angular.module('index', ['ngMap','App.services','ui.bootstrap']);
 
-angular.module("login",[])
+angular.module("login",['LocalStorageModule'])
 
 //.controller('SendLoginCtrl', ['$scope','$http','$window','SharedVars', function($scope, $http, $window, SharedVars) {
-.controller('SendLoginCtrl', ['$scope','$http','$window', function($scope, $http, $window) {//
+.controller('SendLoginCtrl', ['$scope','$http','$window','localStorageService', function($scope, $http, $window, localStorageService) {//   
+  
 	$scope.loginStore = function(store) {
     console.log(JSON.stringify(store));		
 		var email = store.email;
@@ -34,8 +35,9 @@ angular.module("login",[])
       rawJSON = JSON.parse(JSON.stringify(response));
       $scope.store_id = rawJSON.store.store_id;
       console.log(rawJSON.store.token);        
-      console.log(rawJSON.store.store_id);        
-      //SharedVars.setStoreId(rawJSON.store.store_id);
+      console.log(rawJSON.store.store_id);              
+      //Almacenamos el ID del cliente
+      localStorageService.set("Id", rawJSON.store.store_id); 
       $window.location='./views/post_promotion.html'; 
     }).error(function() {
       console.log("otra cosa");
