f6c4b8855068f85241f02e6f3209e343091e8684
Esteban Foronda Sierra <seforonda@gmail.com>
2015-04-28 17:54:14 +0200

    admin branches done

--- js/controllers/admin_branches.js
+++ js/controllers/admin_branches.js
@@ -3,128 +3,9 @@
 angular.module("admin_branches",['ngMap','LocalStorageModule','ui.bootstrap'])
 //angular.module("post_promotion",['ngMap','LocalStorageModule'])
 
-//.controller('SendPromotionCtrl', ['$scope','$http','SharedVars', function($scope, $http, SharedVars) {
-.controller('AdminBranchesCtrl', ['$scope','$http', function($scope, $http) {
-  $scope.modifyBranch = function(branch) {
-    var branch_id = $scope.branch_id;
-    console.log(branch_id);
-    var name = document.getElementById('name').value;
-    var address = document.getElementById('address').value;
-    var phone = document.getElementById('phone').value;
-    var lat = document.getElementById('latitude').value;
-    var lng = document.getElementById('longitude').value;
-    var jsonBranch = {
-      "id": branch_id,
-      "name": name,
-      "address": address,
-      "phone": phone,
-      "latitude": lat,
-      "longitude": lng
-    }
-    var branch = {
-      "branch": jsonBranch
-    }
-    console.log(JSON.stringify(branch));
-    //console.log($scope.listRight.options[0].value);
-    //$http.post(url, branch);
-    sendData(branch, $http);
-  }
-
-  function sendData(data, $http) {
-    var req = {
-      method: 'PATCH',
-      url: 'http://nowerserver.tk/branches',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      data: data
-    }
-    $http(req).success(function(response) {
-      evaluteResponse(response);
-    }).error(function(response) {
-      evaluteResponse(response);
-    });
-  }
-
-  function evaluteResponse(response){
-    console.log(JSON.stringify(response));
-    state = response.success;
-    if(state){
-      $scope.alerts = [{ type: 'success', msg: '¡Sucursal modificada! :)' }];
-      document.forms["createBranchForm"].reset();
-    }else{
-      var errorName = [];
-      var errorMsgs = [];
-      var cont = 0;
-      jQuery.each(response.errors, function(attr, errors) {
-        errorName.push(attr);
-        cont += 1;
-        jQuery.each(errors, function() {
-          errorMsgs.push(this);
-        });
-      });
-      showErrorAlert(errorName, errorMsgs, cont);
-    }
-    window.location.href=window.location.href;
-  }
-
-
-  function showErrorAlert(errorName, errorMsgs, cont){
-    if(cont > 1){
-      $scope.alerts = [{ type: 'danger', msg: "Error: " + errorName[0] + " " + errorMsgs[0]}];
-      for(i = 1; i < errorMsgs.length; i++){
-        $scope.alerts.push({type: 'danger', msg: "Error: " + errorName[i] + " " + errorMsgs[i]});
-      }
-    } else {
-      $scope.alerts = [{ type: 'danger', msg: "Error: " + errorName[0] + " " + errorMsgs[0]}];
-    }
-
-  }
-}])
-
 //.controller('EventArgumentsCtrl', ['$scope','$http','SharedVars', function($scope, $http, SharedVars) {
-.controller('BranchesArgumenstCtrl', ['$scope','$http','localStorageService', function($scope, $http, localStorageService) {
+.controller('AdminBranchesCtrl', ['$scope','$http','localStorageService', function($scope, $http, localStorageService) {
   getData($http);
-  $scope.$on('mapInitialized', function(evt, evtMap) {
-    console.log("Entró al inicializador del mapa");
-    $scope.map = evtMap;
-  });
-  $scope.fillBranchFields = function(){
-    var listOfBranches = document.getElementById('selectBranch');
-    var selectedBranch = listOfBranches.options.selectedIndex;
-    var indexSelectedBranch = listOfBranches.options[selectedBranch].index;
-    var name = $scope.rawJSON.branches[indexSelectedBranch].name;
-    var address = $scope.rawJSON.branches[indexSelectedBranch].address;
-    var latitude = $scope.rawJSON.branches[indexSelectedBranch].latitude;
-    var longitude = $scope.rawJSON.branches[indexSelectedBranch].longitude;
-    var phone = $scope.rawJSON.branches[indexSelectedBranch].phone;
-    document.getElementById('name').value = name;
-    document.getElementById('address').value = address;
-    document.getElementById('latitude').value = latitude;
-    document.getElementById('longitude').value = longitude;
-    document.getElementById('phone').value = phone;
-    createMarker(latitude, longitude);
-    $scope.branch_id = $scope.rawJSON.branches[indexSelectedBranch].id;
-    console.log($scope.rawJSON.branches[0]);
-  }
-  //Esta función pone los marcadores en el mapa
-  function createMarker(lat, long){
-    var myLatlng = new google.maps.LatLng(parseFloat(lat),parseFloat(long));
-    var marker = new google.maps.Marker({position: myLatlng, map: $scope.map, draggable:true});
-    $scope.map.setCenter(myLatlng, 15);
-    google.maps.event.addListener(marker, 'dragend', function(evt) {
-      updateCoordinates(evt.latLng.lat(), evt.latLng.lng());
-    });
-  }
-  function updateCoordinates(lat, lng) {
-    document.getElementById('latitude').value = lat;
-    document.getElementById('longitude').value = lng;
-  }
-//no hace nada pero la tengo ahí por si la necesito
-  function updateCoordinates(lat, lng) {
-    document.getElementById('latitude').value = lat;
-    document.getElementById('longitude').value = lng;
-  }
   //function getData($http, SharedVars) {
   function getData($http) {
     //Llamamos al ID almacenado
@@ -141,28 +22,121 @@ angular.module("admin_branches",['ngMap','LocalStorageModule','ui.bootstrap'])
       console.log("ya");
       console.log(JSON.stringify(response));
       $scope.rawJSON = JSON.parse(JSON.stringify(response));
-      createArray();
+      createBranchesTable();
     }).error(function() {
       console.log("otra cosa");
       $scope.alerts = [{ type: 'danger', msg: 'No cargó las sucursales' }];
     });
   }
   //Crea un array con los branches
-  function createArray(){
+  function createBranchesTable(){
     var listBranches = document.getElementById('selectBranch');
     $scope.branches = [];
+    tableElem = document.getElementById("branches");
     for(i = 0; i < $scope.rawJSON.branches.length; i++){
-      $scope.branches.push($scope.rawJSON.branches[i]);
-      create(listBranches, $scope.rawJSON.branches[i].id, $scope.rawJSON.branches[i].name);
-    }
+      rowElem = document.createElement('tr');
+      var name = $scope.rawJSON.branches[i].name;
+      for (var j = 0; j < 3; j++) {
+        colElem = document.createElement('td');
+        switch(j) {
+          case 0:
+            colElem.appendChild(document.createTextNode(i + 1)); //to print cell number
+            rowElem.appendChild(colElem);
+            break;
+          case 1:
+            colElem.appendChild(document.createTextNode(name)); //to print cell number
+            rowElem.appendChild(colElem);
+            break;
+          case 2:
+            var oImg = document.createElement("img");
+            oImg.setAttribute('src', '../rs/img/edit.png');
+            oImg.setAttribute('alt', 'na');
+            oImg.setAttribute('height', '20');
+            oImg.setAttribute('width', '30');
+            oImg.onclick = modifyItem(rowElem);
+            colElem.appendChild(oImg); //to print cell number
+            rowElem.appendChild(colElem);
+          case 3:
+            var oImg = document.createElement("img");
+            oImg.setAttribute('src', '../rs/img/trash.png');
+            oImg.setAttribute('alt', 'na');
+            oImg.setAttribute('height', '20');
+            oImg.setAttribute('width', '30');
+            oImg.onclick = deleteItem(rowElem);
+            colElem.appendChild(oImg); //to print cell number
+            rowElem.appendChild(colElem);
+          }
+        }
+        tableElem.appendChild(rowElem);
+      }
   }
-  //Mete las branches a la lista tipo interactiva
-  function create(listBoxTo,optionValue,optionDisplayText){
-    var newOption = document.createElement("option");
-    newOption.value = optionValue;
-    newOption.text = optionDisplayText;
-    listBoxTo.add(newOption, null);
-    return true;
+  function deleteItem(row){
+    return function() {
+      var cell = row.getElementsByTagName("td")[0];// if you put 0 here then it will return first column of this row
+      var idRow = cell.innerHTML;
+      var idBranch = $scope.rawJSON.branches[idRow - 1].id;
+      deleteBranch($http, idBranch);
+    };
+  }
+  function modifyItem(row){
+    return function() {
+      var cell = row.getElementsByTagName("td")[0];// if you put 0 here then it will return first column of this row
+      var idRow = cell.innerHTML;
+      var idBranch = $scope.rawJSON.branches[idRow - 1].id;
+      sessionStorage.setItem("selectedBranch", idRow - 1);
+      window.location.href = "modify_branches.html";
+    };
+  }
+  function deleteBranch($http, id) {
+    var req = {
+      method: 'DELETE',
+      //url: 'http://nowerserver.tk/stores/branches/'+SharedVars.getStoreId(),
+      url: 'http://nowerserver.tk/branches/'+ id,
+    }
+    $http(req).success(function(response) {
+      console.log("ya");
+      console.log(JSON.stringify(response));
+      $scope.rawJSON = JSON.parse(JSON.stringify(response));
+      evaluateResponse(response);
+    }).error(function(response) {
+      console.log("otra cosa");
+      evaluateResponse(response);
+      //$scope.alerts = [{ type: 'danger', msg: 'No cargó las sucursales' }];
+    });
+  }
+  function evaluateResponse(response){
+    console.log(JSON.stringify(response));
+    state = response.success;
+    if(state){
+      window.location.href = window.location.href;
+    }else{
+      var errorName = [];
+      var errorMsgs = [];
+      var cont = 0;
+      jQuery.each(response.errors, function(attr, errors) {
+        errorName.push(attr);
+        cont += 1;
+        jQuery.each(errors, function() {
+          errorMsgs.push(this);
+        });
+      });
+      showErrorAlert(errorName, errorMsgs, cont);
+    }
+    $("#alert").ready(function(){
+      $("html, body").delay(0).animate({
+        scrollTop: $('#alert').offset().top - 100
+      }, 0);
+    });
+  }
+  function showErrorAlert(errorName, errorMsgs, cont){
+    if(cont > 1){
+      $scope.alerts = [{ type: 'danger', msg: "Error: " + errorName[0] + " " + errorMsgs[0]}];
+      for(i = 1; i < errorMsgs.length; i++){
+        $scope.alerts.push({type: 'danger', msg: "Error: " + errorName[i] + " " + errorMsgs[i]});
+      }
+    } else {
+      $scope.alerts = [{ type: 'danger', msg: "Error: " + errorName[0] + " " + errorMsgs[0]}];
+    }
   }
 }])
 
