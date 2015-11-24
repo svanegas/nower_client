987597df6cc7cfef86cc42ed195adf1379684707
Santiago Vanegas Gil <savanegasg@gmail.com>
2015-11-23 21:39:46 -0500

    Testing openshift

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var app = express();
 
 app.use(express.static('.'));
 
-var port = process.env.PORT || 3000;
-app.listen(port);
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.listen(port, address);
 
 console.log("Listening on port " + port);
